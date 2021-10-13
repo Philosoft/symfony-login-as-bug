a3dea368305adfb7c7191dbed2537203f9836a54
thephilosoft <thephilosoft@gmail.com>
2021-10-13 03:56:21 -0400

    tests: add tests

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,14 +4,27 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomepageController
+class HomepageController extends AbstractController
 {
     /** @Route("/", name="app__homepage") */
     public function homepage(): Response
     {
         return new Response('Homepage is working');
+    }
+
+    /** @Route("/test-auth", name="app__test-auth") */
+    public function testAuthPage(): Response
+    {
+        $user = $this->getUser();
+        if ($user === null) {
+            throw new AccessDeniedHttpException('You cannot do this');
+        }
+
+        return new Response("Greetings {$user->getUserIdentifier()}");
     }
 }
--- tests/functional/MegaTest.php
+++ tests/functional/MegaTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\functional;
 
+use App\Security\UserProvider;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class MegaTest extends WebTestCase
@@ -13,5 +14,44 @@ class MegaTest extends WebTestCase
         $client = static::createClient();
         $client->request('GET', '/');
         $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testAnon(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/test-auth');
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function testLogin(): void
+    {
+        $client = static::createClient();
+        $userProvider = new UserProvider();
+        $userFoo = $userProvider->loadUserByIdentifier('foo@example.com');
+        $client->loginUser($userFoo);
+
+        $client->request('GET', '/test-auth');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertStringEndsWith($userFoo->getUserIdentifier(), $client->getResponse()->getContent());
+    }
+
+    public function testMultiLogin(): void
+    {
+        $client = static::createClient();
+        $userProvider = new UserProvider();
+        $userFoo = $userProvider->loadUserByIdentifier('foo@example.com');
+        $userBar = $userProvider->loadUserByIdentifier('bar@example.com');
+        $client->loginUser($userFoo);
+
+        $client->request('GET', '/test-auth');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertStringEndsWith($userFoo->getUserIdentifier(), $client->getResponse()->getContent());
+
+        $client->loginUser($userBar);
+        $client->request('GET', '/test-auth');
+        $this->assertResponseIsSuccessful();
+        $this->assertStringEndsWith($userBar->getUserIdentifier(), $client->getResponse()->getContent());
     }
 }
